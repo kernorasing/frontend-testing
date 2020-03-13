0fd958900461585ba78e98bce7fda6a778ee0846
Nanthawut Norasing <kernorasing@gmail.com>
2020-03-14 03:08:43 +0700

    Updates

---
+++ precache-manifest.f07fb298626fe90d7d1841855bd6d61a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bcb49140df50a19523cbb7dc2901135",
+    "url": "/frontend-testing/index.html"
+  },
+  {
+    "revision": "52ab4c7516de3f524e5f",
+    "url": "/frontend-testing/static/css/2.1bd75017.chunk.css"
+  },
+  {
+    "revision": "86db54fb3ea6a49839a8",
+    "url": "/frontend-testing/static/css/main.4f21de2b.chunk.css"
+  },
+  {
+    "revision": "52ab4c7516de3f524e5f",
+    "url": "/frontend-testing/static/js/2.1fa87752.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/frontend-testing/static/js/2.1fa87752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86db54fb3ea6a49839a8",
+    "url": "/frontend-testing/static/js/main.baedf7f1.chunk.js"
+  },
+  {
+    "revision": "25022cdfc07f5a9c0578",
+    "url": "/frontend-testing/static/js/runtime-main.7a19d715.js"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/frontend-testing/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/frontend-testing/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/frontend-testing/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/frontend-testing/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/frontend-testing/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/frontend-testing/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/frontend-testing/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/frontend-testing/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/frontend-testing/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/frontend-testing/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/frontend-testing/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/frontend-testing/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/frontend-testing/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/frontend-testing/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/frontend-testing/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frontend-testing/static/media/logo.5d5d9eef.svg"
+  }
+]);
