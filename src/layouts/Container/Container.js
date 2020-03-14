3a381172ca1e65043a5814dc8334e8d9f78a1e02
Nanthawut Norasing <kernorasing@gmail.com>
2020-03-14 12:34:41 +0700

    add redirect path

--- src/layouts/Container/Container.js
+++ src/layouts/Container/Container.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import MyCars from '../../pages/my_cars/MyCars';
 import History from '../../pages/history/History';
@@ -10,7 +10,7 @@ export default class Container extends React.Component {
         return (
             <Router>
                 <Switch>
-                    <Route exact path="/">
+                    <Route exact path="/mycar">
                         <MyCars />
                     </Route>
                     <Route path="/history">
@@ -19,6 +19,8 @@ export default class Container extends React.Component {
                     <Route path="/map">
                         <Map />
                     </Route>
+                    <Redirect from="/" to="/mycar" />
+                    <Redirect from="*" to="/mycar" />
                 </Switch>
             </Router>
         );
